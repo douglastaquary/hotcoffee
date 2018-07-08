548494297a91b7f5e3dd921c124cbd014bfd6a2f
Mohammad Azam <azamsharp@Mohammads-MacBook-Pro.local>
2018-07-08 17:47:11 -0500

    added uiappearance style

--- HotCoffee/AppDelegate.swift
+++ HotCoffee/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 234/255, green: 181/255, blue: 67/255, alpha: 1.0)
+        
+        UINavigationBar.appearance().tintColor = UIColor.white
+        
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        
         return true
     }
 
